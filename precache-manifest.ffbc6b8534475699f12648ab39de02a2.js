05fcd75c3c2bd3122fc83b23e7728793ade3fdb4
litalsh <lital.shalom@gmail.com>
2021-01-04 16:50:46 +0200

    Updates

---
+++ precache-manifest.ffbc6b8534475699f12648ab39de02a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18de3e72b198572377b73b4eb22f9fd9",
+    "url": "/Rock-Paper-Scissors/index.html"
+  },
+  {
+    "revision": "2192f35a5f5f415c29cc",
+    "url": "/Rock-Paper-Scissors/static/css/main.d7bb1fba.chunk.css"
+  },
+  {
+    "revision": "f8ca08103c5a39e7211a",
+    "url": "/Rock-Paper-Scissors/static/js/2.5fa5f55f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Rock-Paper-Scissors/static/js/2.5fa5f55f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2192f35a5f5f415c29cc",
+    "url": "/Rock-Paper-Scissors/static/js/main.8451df17.chunk.js"
+  },
+  {
+    "revision": "4bc0afdeef60574c8bd3",
+    "url": "/Rock-Paper-Scissors/static/js/runtime-main.f3bf4cca.js"
+  }
+]);
